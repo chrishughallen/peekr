4b124cffc3ddf7f88431bfc726815f48729992da
Chris <chrishughallen@gmail.com>
2025-04-18 15:36:20 +0200

    initial commit

---
+++ lib/peekr.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "peekr/version"
+require_relative "peekr/peek"
+
+module Peekr
+  class Error < StandardError; end
+  # You could add more methods or classes here in the future if needed
+end
---
+++ lib/peekr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Peekr
+  VERSION = "0.1.0"
+end
